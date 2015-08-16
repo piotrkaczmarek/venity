f3fb19a6d78ba3eb957838c4b3a13f87394db63f
Piotr Kaczmarek <piotrkaczmarek1991@gmail.com>
2015-08-16 15:09:28 +0200

    Image asset path fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,6 @@
 if Rails.env.test? || Rails.env.development?
   CarrierWave.configure do |config|
     config.storage = :file
-    config.enable_processing = false
   end
 else
   CarrierWave.configure do |config|
